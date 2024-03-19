c1eb732ad307689ba19571373d2b4d24da8e1fb2
TerryLF <114449536+TerryLF41@users.noreply.github.com>
2024-03-19 18:14:02 +0700

    user

--- api/model/user.go
+++ api/model/user.go
@@ -1,14 +1,12 @@
 package model
 
 type User struct {
-	ID              int         `json:"id"`
-	Name            string      `json:"name"`
-	Email           string      `json:"email"`
+	UserID			int         `json:"userId"`
+	Username        string      `json:"username"`
 	Password        string      `json:"password,omitempty"`
-	Address         string      `json:"address"`
-	TelephoneNo     string      `json:"telephone"`
+	Email           string      `json:"email"`
+	ProfileDesc		string		`json:"profileDesc"`
+	JoinDate		date		`json:"joinDate"`
 	UserType        int         `json:"usertype,omitempty"`
 	BanStatus       bool        `json:"banstatus,omitempty"`
-	TransactionList *Transaction `json:"transactionlist,omitempty"`
-	Cart            *Cart        `json:"cart,omitempty"`
 }
