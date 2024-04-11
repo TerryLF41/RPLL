ac6ad69c382b5c575a0e0a013bfe534f31c83686
TerryLF <114449536+TerryLF41@users.noreply.github.com>
2024-04-11 13:48:39 +0700

    Update thread.go

--- api/model/thread.go
+++ api/model/thread.go
@@ -4,35 +4,33 @@ import "time"
 
 // Thread represents a thread model
 type Thread struct {
-	ThreadNo      int       `json:"threadNo"`
-	TopicNo       int       `json:"topicNo"`
-	ThreadTitle   string    `json:"threadTitle"`
-	ThreadPicture string    `json:"threadPicture"`
-	ThreadDesc    string    `json:"threadDesc"`
-	CreateDate    time.Time `json:"createDate"`
-	BanStatus     bool      `json:"banStatus"`
-	PostList      []Post    `json:"post,omitempty"`
+	ThreadNo    int       `json:"threadNo"`
+	TopicNo     int       `json:"topicNo"`
+	ThreadTitle string    `json:"threadTitle"`
+	ThreadDesc  string    `json:"threadDesc"`
+	CreateDate  time.Time `json:"createDate"`
+	BanStatus   bool      `json:"banStatus"`
+	PostList    []Post    `json:"post,omitempty"`
 }
 
 // ThreadModelFactory interface defines methods for thread model
 type ThreadModelFactory interface {
-	CreateThread(threadNo int, topicNo int, threadTitle, threadPicture, threadDesc string, createDate time.Time, banStatus bool, postList []Post) *Thread
+	CreateThread(threadNo int, topicNo int, threadTitle, threadDesc string, createDate time.Time, banStatus bool, postList []Post) *Thread
 }
 
 // ConcreteThreadModelFactory struct implements ThreadModelFactory interface
 type ConcreteThreadModelFactory struct{}
 
 // CreateThread creates a new thread instance
-func (factory *ConcreteThreadModelFactory) CreateThread(threadNo int, topicNo int, threadTitle, threadPicture, threadDesc string, createDate time.Time, banStatus bool, postList []Post) *Thread {
+func (factory *ConcreteThreadModelFactory) CreateThread(threadNo int, topicNo int, threadTitle, threadDesc string, createDate time.Time, banStatus bool, postList []Post) *Thread {
 	return &Thread{
-		ThreadNo:      threadNo,
-		TopicNo:       topicNo,
-		ThreadTitle:   threadTitle,
-		ThreadPicture: threadPicture,
-		ThreadDesc:    threadDesc,
-		CreateDate:    createDate,
-		BanStatus:     banStatus,
-		PostList:      postList,
+		ThreadNo:    threadNo,
+		TopicNo:     topicNo,
+		ThreadTitle: threadTitle,
+		ThreadDesc:  threadDesc,
+		CreateDate:  createDate,
+		BanStatus:   banStatus,
+		PostList:    postList,
 	}
 }
 
