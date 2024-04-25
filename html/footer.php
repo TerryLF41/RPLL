7a07a638447fcb057a4e3f85a7a40aa5b88a69e8
Jonathan_HA78 <joemaxwell789@gmail.com>
2024-04-25 11:13:55 +0700

    hapus file php

--- html/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <img src="upload/others/chattergrey.png">
-    <h2>Diskusi Tanpa Batas</h2>
-    <div class="footer-text">
-        <p>Copyright 2022. All Rights Reserved.</p>
-    </div>
-</footer>
